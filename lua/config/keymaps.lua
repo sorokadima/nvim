48c633ee306118c55bc29e6011439f4d83d81d30
dmytro_soroka <d.soroka@tconsolidator.com>
2026-08-17 17:38:19 +0300

    add normal mode hotkey

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,6 +6,10 @@
 -- Одинарний Esc і далі йде напряму у вкладену програму (lazygit, fzf, nested nvim тощо).
 vim.keymap.set("t", "<Esc><Esc>", [[<C-\><C-n>]], { desc = "Вийти з термінал-режиму" })
 
+-- Надійний вихід з термінал-режиму, що не залежить від Esc.
+-- Потрібен, бо Claude Code сам перехоплює Esc, і подвійний Esc до нього не завжди встигає.
+vim.keymap.set("t", "<C-q>", [[<C-\><C-n>]], { desc = "Вийти з термінал-режиму (Ctrl-q)" })
+
 -- Швидкий запуск Claude Code у плаваючому терміналі (toggle тим самим хоткеєм)
 vim.keymap.set("n", "<leader>ac", function()
   Snacks.terminal("claude", { cwd = LazyVim.root(), win = { position = "float" } })
