385b1b73f5e0c436cd3873349c70b5e62d91d4ba
dima <dmtsoroka@gmail.com>
2026-08-17 11:22:22 +0300

    colorscheme: switch to modus_vivendi (emacs port, less blue)

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,14 +2,17 @@ return {
   -- Disable default LazyVim theme
   { "folke/tokyonight.nvim", enabled = false },
 
-  -- Install Catppuccin theme
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000, lazy = false },
+  -- Catppuccin лишається встановленим, але не активний
+  { "catppuccin/nvim", name = "catppuccin", lazy = true },
+
+  -- Modus Vivendi (порт вбудованої emacs-теми) — чорний фон, мінімум синього
+  { "miikanissi/modus-themes.nvim", priority = 1000, lazy = false },
 
   -- Set it as default colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "modus_vivendi",
     },
   },
 }
